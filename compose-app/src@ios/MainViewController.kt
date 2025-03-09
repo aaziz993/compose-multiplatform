211e9e931e9e697d038fa3af7ee1db0f47daf2ed
azizatoev <a.atoev93@gmail.com>
2025-03-09 14:02:56 +0500

    Initial Commit

--- compose-app/src@ios/MainViewController.kt
+++ compose-app/src@ios/MainViewController.kt
@@ -1,7 +1,7 @@
 import androidx.compose.ui.window.ComposeUIViewController
 import platform.UIKit.UIViewController
 
-public fun ViewController(): UIViewController = ComposeUIViewController({
+public fun MainViewController(): UIViewController = ComposeUIViewController({
     // Since 1.7.0-beta01 you have to add CADisableMinimumFrameDurationOnPhone property in your info.plist
     //release notes: https://github.com/JetBrains/compose-multiplatform/releases/tag/v1.7.0-beta01
     // The app will crash by default, if CADisableMinimumFrameDurationOnPhone is not set to true in Info.plist.
--- iosApp/iosApp/ContentView.swift
+++ iosApp/iosApp/ContentView.swift
@@ -1,6 +1,6 @@
 import UIKit
 import SwiftUI
-import ComposeApp
+import compose-app
 
 struct ComposeView: UIViewControllerRepresentable {
     func makeUIViewController(context: Context) -> UIViewController {
