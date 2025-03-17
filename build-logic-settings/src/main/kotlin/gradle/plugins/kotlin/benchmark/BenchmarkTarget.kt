13fbb51925631563c7305742e5d57a06f097b545
azizatoev <a.atoev93@gmail.com>
2025-03-17 15:59:21 +0500

    Initial Commit

--- build-logic-settings/src/main/kotlin/gradle/plugins/kotlin/benchmark/BenchmarkTarget.kt
+++ build-logic-settings/src/main/kotlin/gradle/plugins/kotlin/benchmark/BenchmarkTarget.kt
@@ -1,11 +1,13 @@
 package gradle.plugins.kotlin.benchmark
 
 import gradle.api.trySet
+import gradle.plugins.kmp.KotlinTarget
+import gradle.serialization.serializer.JsonPolymorphicSerializer
 import gradle.serialization.serializer.KeyTransformingSerializer
 import kotlinx.serialization.Serializable
 import org.gradle.api.Project
 
-@Serializable
+@Serializable(with = BenchmarkTargetSerializer::class)
 internal abstract class BenchmarkTarget {
 
     abstract val name: String
@@ -16,6 +18,10 @@ internal abstract class BenchmarkTarget {
         target::workingDir trySet workingDir
     }
 }
+
+private object BenchmarkTargetSerializer : JsonPolymorphicSerializer<BenchmarkTarget>(
+    BenchmarkTarget::class,
+)
 
 internal object BenchmarkTargetTransformingSerializer : KeyTransformingSerializer<BenchmarkTarget>(
     BenchmarkTarget.serializer(),
