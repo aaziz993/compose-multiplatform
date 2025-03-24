a4ed8f9f352665a72fc4028fb2afc061c37d7f21
azizatoev <a.atoev93@gmail.com>
2025-03-24 16:47:39 +0500

    Initial Commit

--- build-logic-settings/src/main/kotlin/gradle/plugins/kmp/nat/KotlinNativeBinaryTestRun.kt
+++ build-logic-settings/src/main/kotlin/gradle/plugins/kmp/nat/KotlinNativeBinaryTestRun.kt
@@ -2,11 +2,35 @@ package gradle.plugins.kmp.nat
 
 import gradle.api.tasks.test.TestFilter
 import gradle.plugins.kmp.KotlinTargetTestRun
+import gradle.plugins.kotlin.KotlinExecution
 import gradle.serialization.serializer.KeyTransformingSerializer
 import kotlinx.serialization.Serializable
+import org.gradle.api.Project
+import org.jetbrains.kotlin.gradle.plugin.mpp.KotlinNativeTargetWithTests
 
 internal interface KotlinNativeBinaryTestRun<T : org.jetbrains.kotlin.gradle.targets.native.KotlinNativeBinaryTestRun>
     : KotlinTargetTestRun<T> {
+
+    override val executionSource: NativeBinaryTestRunSource?
+
+    context(Project)
+    override fun applyTo(receiver: T) {
+        super.applyTo(receiver)
+
+        val target = receiver.target as KotlinNativeTargetWithTests<*>
+
+        when (val binary = executionSource.binary) {
+            is String -> receiver.setExecutionSourceFrom(
+                target.binaries.getTest(binary),
+            )
+
+            is Binary -> receiver.setExecutionSourceFrom(
+                target.binaries.getTest(binary.namePrefix, binary.buildType),
+            )
+
+            else -> Unit
+        }
+    }
 }
 
 @Serializable
--- build-logic-settings/src/main/kotlin/gradle/plugins/kmp/nat/NativeBinaryTestRunSource.kt
+++ build-logic-settings/src/main/kotlin/gradle/plugins/kmp/nat/NativeBinaryTestRunSource.kt
@@ -7,24 +7,6 @@ import org.jetbrains.kotlin.gradle.plugin.mpp.KotlinNativeTargetWithTests
 import org.jetbrains.kotlin.gradle.targets.native.KotlinNativeBinaryTestRun
 
 @Serializable
-internal data class NativeBinaryTestRunSource<T : KotlinNativeBinaryTestRun>(
+internal data class NativeBinaryTestRunSource(
     val binary: @Serializable(with = BinaryContentPolymorphicSerializer::class) Any,
-) : KotlinExecution.ExecutionSource<T> {
-
-    context(Project)
-    override fun applyTo(receiver: T) {
-        val target = receiver.target as KotlinNativeTargetWithTests<*>
-
-        when (binary) {
-            is String -> receiver.setExecutionSourceFrom(
-                target.binaries.getTest(binary),
-            )
-
-            is Binary -> receiver.setExecutionSourceFrom(
-                target.binaries.getTest(binary.namePrefix, binary.buildType),
-            )
-
-            else -> Unit
-        }
-    }
-}
+) : KotlinExecution.ExecutionSource
