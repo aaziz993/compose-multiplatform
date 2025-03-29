1c51b34bfc4cf63283728e68a3c97d24f2736275
azizatoev <a.atoev93@gmail.com>
2025-03-30 02:30:59 +0500

    Initial Commit

--- build-logic-settings/src/main/kotlin/gradle/serialization/serializer/JsonBaseValueTransformingContentPolymorphicSerializer.kt
+++
@@ -1,9 +0,0 @@
-package gradle.serialization.serializer
-
-import kotlin.reflect.KClass
-import kotlinx.serialization.json.JsonTransformingSerializer
-
-public abstract class JsonBaseValueTransformingContentPolymorphicSerializer<T : Any>(
-    baseClass: KClass<T>,
-    classDiscriminator: String = "type",
-) : JsonTransformingSerializer<T>(JsonContentPolymorphicSerializer(baseClass, classDiscriminator))
