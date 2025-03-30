e074112158f6971a923405a6f90cc6e5c6263f3c
azizatoev <a.atoev93@gmail.com>
2025-03-30 20:29:30 +0500

    Initial Commit

--- build-logic-settings/src/main/kotlin/gradle/plugins/initialization/IncludeBuild.kt
+++ build-logic-settings/src/main/kotlin/gradle/plugins/initialization/IncludeBuild.kt
@@ -1,10 +1,19 @@
 package gradle.plugins.initialization
 
 import gradle.api.initialization.ConfigurableIncludedBuild
+import gradle.serialization.serializer.JsonObjectTransformingSerializer
+import kotlinx.serialization.KeepGeneratedSerializer
 import kotlinx.serialization.Serializable
 
-@Serializable
+@KeepGeneratedSerializer
+@Serializable(with = IncludeBuildObjectTransformingSerializer::class)
 internal data class IncludeBuild(
     val rootProject: String,
     val configuration: ConfigurableIncludedBuild? = null,
+)
+
+private object IncludeBuildObjectTransformingSerializer
+    : JsonObjectTransformingSerializer<IncludeBuild>(
+    IncludeBuild.serializer(),
+    "rootProject",
 )
