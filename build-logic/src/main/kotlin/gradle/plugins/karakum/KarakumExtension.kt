916e7a368c5305b384ea119ea6b7ce2bfce985a4
azizatoev <a.atoev93@gmail.com>
2025-09-08 15:10:53 +0500

    Initial Commit

--- build-logic/src/main/kotlin/gradle/plugins/karakum/KarakumExtension.kt
+++ build-logic/src/main/kotlin/gradle/plugins/karakum/KarakumExtension.kt
@@ -1,25 +1,35 @@
 package gradle.plugins.karakum
 
+import gradle.api.configureEach
+import gradle.api.project.kotlin
 import io.github.sgrishchenko.karakum.gradle.plugin.KarakumExtension
+import java.io.File
 import klib.data.type.collections.map.asMap
 import klib.data.type.serialization.json.decodeAnyFromString
 import kotlinx.serialization.json.Json
 import org.gradle.api.Project
-import org.jetbrains.kotlin.gradle.plugin.KotlinSourceSet
-import java.io.File
+import org.gradle.kotlin.dsl.withType
+import org.jetbrains.kotlin.gradle.targets.js.dsl.KotlinJsTargetDsl
 
-@Suppress("UnusedReceiverParameter")
 context(project: Project)
-public fun KarakumExtension.sourceSets(vararg sourceSets: KotlinSourceSet): Unit =
-    project.pluginManager.withPlugin("io.github.sgrishchenko.karakum") {
-        project.file("karakum.config.json")
-            .takeIf(File::exists)
-            ?.let(File::readText)
-            ?.let(Json.Default::decodeAnyFromString)
-            ?.asMap
-            ?.let { karakumConfig ->
-                sourceSets.forEach { sourceSet ->
+public fun KarakumExtension.sourceSets(
+    vararg sourceSetNames: String = project.kotlin.targets
+        .withType<KotlinJsTargetDsl>()
+        .filter { this::class == KotlinJsTargetDsl::class }
+        .flatMap { target ->
+            listOf("Main", "Test").map { compilationName -> "${target.name}$compilationName" }
+        }.toTypedArray()
+): Unit = project.pluginManager.withPlugin("io.github.sgrishchenko.karakum") {
+    project.file("karakum.config.json")
+        .takeIf(File::exists)
+        ?.let(File::readText)
+        ?.let(Json.Default::decodeAnyFromString)
+        ?.asMap
+        ?.let { karakumConfig ->
+            project.kotlin.sourceSets
+                .matching { sourceSet -> sourceSet.name in sourceSetNames }
+                .configureEach { sourceSet ->
                     sourceSet.kotlin.srcDir(project.file(karakumConfig["output"]!!))
                 }
-            }
-    }
+        }
+}
