4332e5ae0d48146fa8ae426d08992c448e4b81d6
azizatoev <a.atoev93@gmail.com>
2025-09-18 03:01:37 +0500

    Initial Commit

--- klib/src@jvm/ApplicationEngine.kt
+++ klib/src@jvm/ApplicationEngine.kt
@@ -2,41 +2,40 @@ import io.ktor.network.tls.certificates.buildKeyStore
 import io.ktor.network.tls.certificates.saveToFile
 import io.ktor.network.tls.extensions.HashAlgorithm
 import io.ktor.network.tls.extensions.SignatureAlgorithm
+import io.ktor.server.engine.ApplicationEngine
 import java.io.File
 import java.io.FileInputStream
 import java.net.Inet4Address
 import java.security.KeyStore
 import javax.security.auth.x500.X500Principal
 
-@Suppress("UnusedReceiverParameter")
 public fun ApplicationEngine.Configuration.generateSelfSignedSSL(
-    val keyStoreFile: String = File("build/keystore.p12"),
-    val keyStorePassword: String,
-    val keyAlias: String = "serverSSL",
-    val hash: String? = null,
-    val sign: String? = null,
-    val privateKeyPassword: String,
-    val subject: String? = null,
-    val daysValid: Long? = null,
-    val keySizeInBits: Int? = null,
-    val domains: List<String>? = null,
-    val ipAddresses: List<String>? = null,
-    val format: String = "PKCS12",
-    val rewrite: Boolean = false,
-    val port: Int = 443,
+    keyStoreFile: File = File("build/keystore.p12"),
+    keyStorePassword: String,
+    keyAlias: String = "serverSSL",
+    hash: String? = null,
+    sign: String? = null,
+    privateKeyPassword: String,
+    subject: String? = null,
+    daysValid: Long? = null,
+    keySizeInBits: Int? = null,
+    domains: List<String>? = null,
+    ipAddresses: List<String>? = null,
+    format: String = "PKCS12",
+    rewrite: Boolean = false,
 ): KeyStore = if ((keyStoreFile.exists() && !rewrite))
     KeyStore.getInstance(format).apply {
         load(FileInputStream(keyStoreFile), keyStorePassword.toCharArray())
     }
 else buildKeyStore {
     certificate(keyAlias) {
-        this@generateSelfSignedSSL.hash?.let { hash = HashAlgorithm.valueOf(it) }
-        this@generateSelfSignedSSL.sign?.let { sign = SignatureAlgorithm.valueOf(it) }
-        this@generateSelfSignedSSL = privateKeyPassword
-        this@generateSelfSignedSSL.subject?.let { subject = X500Principal(it) }
-        this@generateSelfSignedSSL.daysValid?.let { daysValid = it }
-        this@generateSelfSignedSSL.keySizeInBits?.let { keySizeInBits = it }
-        this@generateSelfSignedSSL.domains?.let { domains = it }
-        this@generateSelfSignedSSL.ipAddresses?.let { ipAddresses = it.map(Inet4Address::getByName) }
+        hash?.let { this.hash = HashAlgorithm.valueOf(it) }
+        sign?.let { this.sign = SignatureAlgorithm.valueOf(it) }
+        this.password = privateKeyPassword
+        subject?.let { this.subject = X500Principal(it) }
+        daysValid?.let { this.daysValid = it }
+        keySizeInBits?.let { this.keySizeInBits = it }
+        domains?.let { this.domains = it }
+        ipAddresses?.let { this.ipAddresses = it.map(Inet4Address::getByName) }
     }
 }.also { keyStore -> keyStore.saveToFile(keyStoreFile, keyStorePassword) }
