0b507b5add11b1f05ac2bf9cd838577c5a8e6a02
azizatoev <a.atoev93@gmail.com>
2025-09-19 13:20:54 +0500

    Initial Commit

--- build-logic/src/main/kotlin/gradle/plugins/signing/SigningExtension.kt
+++ build-logic/src/main/kotlin/gradle/plugins/signing/SigningExtension.kt
@@ -4,85 +4,124 @@ import gradle.api.initialization.settingsScript
 import gradle.api.project.sensitive
 import gradle.api.project.sensitiveOrElse
 import gradle.api.project.settings
+import gradle.api.project.signing
+import java.io.ByteArrayOutputStream
+import java.io.File
+import java.util.Date
+import org.bouncycastle.bcpg.ArmoredOutputStream
+import org.bouncycastle.openpgp.PGPSecretKeyRing
 import org.gradle.api.Project
 import org.gradle.api.tasks.Exec
 import org.gradle.kotlin.dsl.register
 import org.gradle.plugins.signing.SigningExtension
 import org.gradle.process.CommandLineArgumentProvider
+import org.pgpainless.PGPainless
+import org.pgpainless.PGPainless.Companion.buildKeyRing
+import org.pgpainless.algorithm.KeyFlag
+import org.pgpainless.key.generation.KeySpec.Companion.getBuilder
+import org.pgpainless.key.generation.type.KeyType
+import org.pgpainless.key.generation.type.ecc.EllipticCurve
+import org.pgpainless.key.generation.type.eddsa_legacy.EdDSALegacyCurve
+import org.pgpainless.key.generation.type.rsa.RsaLength
+import org.pgpainless.key.generation.type.xdh_legacy.XDHLegacySpec
+import org.pgpainless.util.Passphrase
 
 @Suppress("UnusedReceiverParameter")
 context(project: Project)
-public fun SigningExtension.registerGPGTasks(): Unit = project.pluginManager.withPlugin("signing") {
-    val passphrase = project.sensitive("signing.gnupg.passphrase")
+public fun SigningExtension.gpg(): Unit = project.pluginManager.withPlugin("signing") {
+    val password = project.sensitive("signing.gnupg.password")
     val keyType = project.sensitiveOrElse("signing.gnupg.key.type") { "RSA" }
-    val keyLength = project.sensitiveOrElse("signing.gnupg.key.length") { "4096" }.toInt()
+    val keyParam = project.sensitiveOrElse("signing.gnupg.key.param") { "4096" }
     val subkeyType = project.sensitiveOrElse("signing.gnupg.subkey.type") { "RSA" }
-    val subkeyLength = project.sensitiveOrElse("signing.gnupg.subkey.length") { "4096" }.toInt()
+    val subkeyParam = project.sensitiveOrElse("signing.gnupg.subkey.param") { "4096" }
     val nameReal = project.sensitiveOrElse("signing.gnupg.name.real") {
         project.settings.settingsScript.developer.name!!
     }
-    val nameComment = project.sensitiveOrElse("signing.gnupg.name.comment") { project.description.orEmpty() }
     val nameEmail = project.sensitiveOrElse("signing.gnupg.name.email") {
         project.settings.settingsScript.developer.email!!
     }
-    val expireDate = project.sensitiveOrElse("signing.gnupg.expiryDate") { "0" }.toLong()
+    val nameComment = project.sensitiveOrElse("signing.gnupg.name.comment") { project.description.orEmpty() }
+    val expire = project.sensitiveOrElse("signing.gnupg.expire") { "0" }.toLong()
 
-    /** Distribute signing gpg key
-     * There are 3 servers supported by Central servers: [ keyserver.ubuntu.com, keys.openpgp.org, pgp.mit.edu ]
-     */
-    project.tasks.register<Exec>("gpgGenKey") {
-        description = "Generates the signing GPG key"
-        group = "signing"
+    val keyFile = File(project.projectDir, "signing.asc")
 
-        executable = project.settings.settingsDir.resolve("scripts/gpg/gpg-gen-key.sh").absolutePath
-
-        args(
-            passphrase,
+    val key = keyFile.takeIf(File::exists)?.readText()
+        ?: PGPainless.modernKeyRing(
+            password,
             keyType,
-            keyLength,
+            keyParam,
             subkeyType,
-            subkeyLength,
+            subkeyParam,
             nameReal,
-            nameComment,
             nameEmail,
-            expireDate,
-        )
-    }
+            nameComment,
+            expire,
+        ).toAsciiArmored().also(keyFile::writeText)
 
-    project.tasks.register<Exec>("gpgSecretKeyList") {
-        description = "List the signing GPG keys"
-        group = "signing"
-
-        executable = project.settings.settingsDir.resolve("scripts/gpg/gpg-secret-key-list.sh").absolutePath
-
-        args(passphrase, nameReal)
-    }
-
-    project.tasks.register<Exec>("gpgCleanKeys") {
-        description = "Clean the signing GPG keys"
-        group = "signing"
-
-        executable = project.settings.settingsDir.resolve("scripts/gpg/gpg-clean-keys.sh").absolutePath
-
-        args(nameReal)
-    }
-
-    project.tasks.register<Exec>("gpgExportKey") {
-        description = "Export the signing GPG key to servers: [keyserver.ubuntu.com, keys.openpgp.org, pgp.mit.edu]"
-        group = "signing"
-
-        executable = project.settings.settingsDir.resolve("scripts/gpg/gpg-export-key.sh").absolutePath
-
-        argumentProviders.add(
-            CommandLineArgumentProvider {
-                listOf(
-                    project.sensitiveOrElse("signing.gnupg.key.servers") { "" },
-                    project.sensitive("signing.gnupg.key"),
-                )
-            },
-        )
-    }
+    project.signing.useInMemoryPgpKeys(key, password)
 }
 
+public fun PGPainless.Companion.modernKeyRing(
+    passphrase: String = "",
+    keyType: String = "RSA",
+    keyParam: String = "4096",
+    subkeyType: String = "RSA",
+    subkeyParam: String = "4096",
+    nameReal: String = "",
+    nameEmail: String = "",
+    nameComment: String = "",
+    expire: Long = 0L
+): PGPSecretKeyRing =
+    buildKeyRing()
+        .apply {
+            setPrimaryKey(
+                getBuilder(
+                    keyType.toKeyType(keyParam), KeyFlag.CERTIFY_OTHER,
+                ),
+            )
+            addSubkey(
+                getBuilder(
+                    subkeyType.toKeyType(subkeyParam),
+                    KeyFlag.ENCRYPT_COMMS,
+                    KeyFlag.ENCRYPT_STORAGE,
+                ),
+            )
+            addSubkey(
+                getBuilder(KeyType.EDDSA_LEGACY(EdDSALegacyCurve._Ed25519), KeyFlag.SIGN_DATA),
+            )
+            setPassphrase(Passphrase(passphrase.toCharArray()))
+
+            if (nameEmail.isNotBlank()) {
+                val userId = buildString {
+                    append(nameReal)
+                    if (nameComment.isNotBlank()) append(" ($nameComment)")
+                    append(" <$nameEmail>")
+                }
+                addUserId(userId)
+            }
+
+            // Set expiration if > 0
+            if (expire > 0L) {
+                val expireDate = Date(System.currentTimeMillis() + expire * 1000)
+                setExpirationDate(expireDate)
+            }
+        }
+        .build()
+
+public fun String.toKeyType(arg: String): KeyType = when (this) {
+    "EDDSA_LEGACY" -> KeyType.EDDSA_LEGACY(EdDSALegacyCurve.valueOf("_$arg"))
+    "ECDSA" -> KeyType.ECDSA(EllipticCurve.valueOf("_$arg"))
+    "XDH_LEGACY" -> KeyType.XDH_LEGACY(XDHLegacySpec.valueOf("_$arg"))
+    "RSA" -> KeyType.RSA(RsaLength.valueOf("_$arg"))
+
+    else -> throw IllegalArgumentException("Unknown key type: $this")
+}
+
+public fun PGPSecretKeyRing.toAsciiArmored(): String = ByteArrayOutputStream().run {
+    ArmoredOutputStream(this).use { armor ->
+        encode(armor)
+    }
+    toString(Charsets.UTF_8.name())
+}
 
 
