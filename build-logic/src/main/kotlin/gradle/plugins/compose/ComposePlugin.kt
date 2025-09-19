4911c9aa90e5253108b151d0992e7ba218f24845
azizatoev <a.atoev93@gmail.com>
2025-09-19 19:25:46 +0500

    Initial Commit

--- build-logic/src/main/kotlin/gradle/plugins/compose/ComposePlugin.kt
+++ build-logic/src/main/kotlin/gradle/plugins/compose/ComposePlugin.kt
@@ -2,12 +2,15 @@ package gradle.plugins.compose
 
 import gradle.api.project.ProjectLayout
 import gradle.api.project.compose
+import gradle.api.project.desktop
 import gradle.api.project.kotlin
 import gradle.api.project.projectScript
 import gradle.api.project.resources
 import gradle.api.project.sourceSetsToComposeResourcesDirs
+import klib.data.type.reflection.invoke
 import org.gradle.api.Plugin
 import org.gradle.api.Project
+import org.gradle.kotlin.dsl.assign
 
 public class ComposePlugin : Plugin<Project> {
 
@@ -20,15 +23,33 @@ public class ComposePlugin : Plugin<Project> {
     }
 
     private fun Project.adjustResources() = project.pluginManager.withPlugin("org.jetbrains.compose") {
+        var dir = "src/commonMain/composeResources"
         when (project.projectScript.layout) {
-            is ProjectLayout.Flat -> kotlin.sourceSets.forEach { sourceSet ->
-                compose.resources.customDirectory(
-                    sourceSet.name,
-                    project.provider { sourceSetsToComposeResourcesDirs[sourceSet]!! },
-                )
+            is ProjectLayout.Flat -> {
+                kotlin.sourceSets.forEach { sourceSet ->
+                    compose.resources.customDirectory(
+                        sourceSet.name,
+                        project.provider { sourceSetsToComposeResourcesDirs[sourceSet]!! },
+                    )
+                }
+                dir = "composeResources"
             }
 
             else -> Unit
+        }
+
+        // jpackage only supports .png on Linux, .ico on Windows, .icns on Mac, so a developer must do a conversion (probably from a png) to a 3 different formats.
+        // Also it seems that ico and icns need to contain an icon in multiple resolutions, so the conversion becomes a bit inconvenient.
+        compose.desktop.application.nativeDistributions {
+            macOS {
+                if (!iconFile.isPresent) iconFile = project.file("$dir/drawable/compose-multiplatform.icns")
+            }
+            linux {
+                if (!iconFile.isPresent) iconFile = project.file("$dir/drawable/compose-multiplatform.png")
+            }
+            windows {
+                if (!iconFile.isPresent) iconFile = project.file("$dir/drawable/compose-multiplatform.ico")
+            }
         }
     }
 }
