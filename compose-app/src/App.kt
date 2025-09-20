93fbf37131f676fd47cb3bd36146c6b462a3fb24
azizatoev <a.atoev93@gmail.com>
2025-09-20 17:12:50 +0500

    Initial Commit

--- compose-app/src/App.kt
+++ compose-app/src/App.kt
@@ -15,6 +15,8 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.navigation.NavController
 import androidx.navigation.compose.rememberNavController
+import compose_app.generated.resources.Res
+import compose_app.generated.resources.compose_multiplatform
 import org.jetbrains.compose.resources.painterResource
 import org.jetbrains.compose.ui.tooling.preview.Preview
 
@@ -37,7 +39,7 @@ public fun App(
             AnimatedVisibility(showContent) {
                 val greeting = remember { "Some" }
                 Column(Modifier.fillMaxWidth(), horizontalAlignment = Alignment.CenterHorizontally) {
-//                    Image(painterResource(Res.drawable.compose_multiplatform), null)
+                    Image(painterResource(Res.drawable.compose_multiplatform), null)
                     Text("Compose: $greeting")
                 }
             }
