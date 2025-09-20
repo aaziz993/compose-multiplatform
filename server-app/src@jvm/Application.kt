061282895b8113eb2fbc13ae32594443e0909ae6
azizatoev <a.atoev93@gmail.com>
2025-09-21 02:11:21 +0500

    Initial Commit

--- server-app/src@jvm/Application.kt
+++ server-app/src@jvm/Application.kt
@@ -6,13 +6,16 @@ import config.ServerConfig
 import engine.Netty
 import io.github.smiley4.ktoropenapi.get
 import io.ktor.http.*
+import io.ktor.http.content.*
 import io.ktor.server.application.*
 import io.ktor.server.netty.*
+import io.ktor.server.plugins.conditionalheaders.ConditionalHeaders
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
-import java.time.format.DateTimeFormatterBuilder
-import java.time.format.SignStyle
-import java.time.temporal.ChronoField
+import java.io.File
+import java.time.format.*
+import java.time.temporal.*
+import java.util.*
 import kotlinx.coroutines.awaitCancellation
 
 public fun main(): Unit = SuspendApp {
@@ -36,6 +39,18 @@ public fun main(): Unit = SuspendApp {
 
 @Suppress("unused")
 private fun Application.module() {
+//    install(ConditionalHeaders) {
+//        val file = File("src@jvm/Application.kt")
+//        version { call, outgoingContent ->
+//            when (outgoingContent.contentType?.withoutParameters()) {
+//                ContentType.Text.CSS -> listOf(
+//                    EntityTagVersion(file.lastModified().hashCode().toString()),
+//                    LastModifiedVersion(Date(file.lastModified()))
+//                )
+//                else -> emptyList()
+//            }
+//        }
+//    }
 }
 
 public fun Application.ping(): Routing = routing {
