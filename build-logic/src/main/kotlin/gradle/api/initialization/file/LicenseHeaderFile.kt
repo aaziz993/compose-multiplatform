1f1089d19a4ff2af605d92ca7e1e66bf1c717ecc
azizatoev <a.atoev93@gmail.com>
2025-09-21 20:44:17 +0500

    Initial Commit

--- build-logic/src/main/kotlin/gradle/api/initialization/file/LicenseHeaderFile.kt
+++ build-logic/src/main/kotlin/gradle/api/initialization/file/LicenseHeaderFile.kt
@@ -5,6 +5,8 @@ import kotlinx.serialization.Serializable
 import kotlinx.serialization.Transient
 import org.gradle.api.initialization.Settings
 
+public const val LICENSE_HEADER_FILE_NAME: String = "LICENSE_HEADER"
+
 @Serializable
 public data class LicenseHeaderFile(
     val source: String,
@@ -15,7 +17,7 @@ public data class LicenseHeaderFile(
     override val from: List<String> = listOf(source)
 
     @Transient
-    override val into: String = "licenses/LICENSE_HEADER"
+    override val into: String = "licenses"
 
     @Suppress("UnstableApiUsage")
     context(settings: Settings)
@@ -30,10 +32,9 @@ public data class LicenseHeaderFile(
 
         val create = previousLicenseText == null || previousLicenseText != licenseText
 
-        val slashLicenseFile = settings.layout.settingsDirectory.file("SLASH_$into").asFile
-        val hashLicenseFile = settings.layout.settingsDirectory.file("HASH_$into").asFile
-
-        val tagLicenseFile = settings.layout.settingsDirectory.file("TAG_$into").asFile
+        val slashLicenseFile = settings.layout.settingsDirectory.file(into).asFile.resolve("SLASH_$LICENSE_HEADER_FILE_NAME")
+        val hashLicenseFile = settings.layout.settingsDirectory.file(into).asFile.resolve("HASH_$LICENSE_HEADER_FILE_NAME")
+        val tagLicenseFile = settings.layout.settingsDirectory.file(into).asFile.resolve("TAG_$LICENSE_HEADER_FILE_NAME")
 
         if (create || !slashLicenseFile.exists())
             slashLicenseFile.writeText(
