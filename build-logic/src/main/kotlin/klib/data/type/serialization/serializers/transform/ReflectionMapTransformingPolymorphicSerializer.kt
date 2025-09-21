d6aa7356cbf8de727dd55240652010aec1988bb7
azizatoev <a.atoev93@gmail.com>
2025-09-21 23:38:36 +0500

    Initial Commit

--- build-logic/src/main/kotlin/klib/data/type/serialization/serializers/transform/ReflectionMapTransformingPolymorphicSerializer.kt
+++ build-logic/src/main/kotlin/klib/data/type/serialization/serializers/transform/ReflectionMapTransformingPolymorphicSerializer.kt
@@ -2,14 +2,17 @@ package klib.data.type.serialization.serializers.transform
 
 import klib.data.type.serialization.serializers.polymorphic.ReflectionPolymorphicSerializer
 import kotlin.reflect.KClass
+import kotlinx.serialization.KSerializer
 
 public open class ReflectionMapTransformingPolymorphicSerializer<T : Any>(
     baseClass: KClass<T>,
+    baseSerializer: KSerializer<T>? = null,
     classDiscriminator: String = "type",
     valueDiscriminator: String = "value"
 ) : MapTransformingSerializer<T>(
     ReflectionPolymorphicSerializer(
         baseClass,
+        baseSerializer,
         classDiscriminator,
         valueDiscriminator,
     ),
