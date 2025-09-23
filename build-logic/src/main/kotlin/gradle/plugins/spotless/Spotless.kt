cf45967f3244d9e5390cb44caa62790a6a6e122d
azizatoev <a.atoev93@gmail.com>
2025-09-23 16:32:57 +0500

    Initial Commit

--- build-logic/src/main/kotlin/gradle/plugins/spotless/Spotless.kt
+++ build-logic/src/main/kotlin/gradle/plugins/spotless/Spotless.kt
@@ -3,10 +3,13 @@ package gradle.plugins.spotless
 import com.diffplug.gradle.spotless.FormatExtension
 import com.diffplug.gradle.spotless.SpotlessExtension
 import gradle.api.project.spotless
-import org.gradle.api.Project
 import kotlin.reflect.full.memberProperties
 import kotlin.reflect.jvm.isAccessible
+import org.gradle.api.Project
 
+public const val JS_LICENSE_HEADER_DELIMITER: String = """(^("|')use (strict|client)("|'))|(import|export|class|function|interface|type|enum|namespace|module) """
+
+@Suppress("UnusedReceiverParameter", "UNCHECKED_CAST")
 context(project: Project)
 public fun SpotlessExtension.formats(block: FormatExtension.() -> Unit): Unit =
     project.spotless::class.memberProperties
