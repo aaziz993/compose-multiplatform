8666b111e211ad2ec2963841106aa3fb87cfdab0
azizatoev <a.atoev93@gmail.com>
2025-09-23 21:00:28 +0500

    Initial Commit

--- build-logic/src/main/kotlin/gradle/plugins/compose/ComposePlugin.kt
+++ build-logic/src/main/kotlin/gradle/plugins/compose/ComposePlugin.kt
@@ -10,6 +10,15 @@ import gradle.api.project.kotlin
 import gradle.api.project.projectScript
 import gradle.api.project.resources
 import gradle.api.project.sourceSetsToComposeResourcesDirs
+import java.io.File
+import java.io.FileInputStream
+import java.io.FileOutputStream
+import javax.imageio.ImageIO
+import org.apache.batik.transcoder.TranscoderInput
+import org.apache.batik.transcoder.TranscoderOutput
+import org.apache.batik.transcoder.image.PNGTranscoder
+import org.apache.commons.imaging.ImageFormats
+import org.apache.commons.imaging.Imaging
 import org.gradle.api.Plugin
 import org.gradle.api.Project
 import org.gradle.kotlin.dsl.assign
@@ -20,14 +29,28 @@ import org.jetbrains.compose.resources.getPreparedComposeResourcesDir
 import org.jetbrains.kotlin.gradle.plugin.KotlinTarget
 import org.jetbrains.kotlin.gradle.targets.jvm.KotlinJvmTarget
 
+private val DENSITIES = mapOf(
+    "ldpi" to 18,
+    "mdpi" to 24,
+    "hdpi" to 36,
+    "xhdpi" to 48,
+    "xxhdpi" to 72,
+    "xxxhdpi" to 96,
+)
+
+private val ICNS_SIZES = listOf(16, 32, 64, 128, 256, 512, 1024)
+
+private val ICO_SIZES = listOf(16, 24, 32, 48, 64, 128, 256)
+
+private const val COMPOSE_MULTIPLATFORM_ICON_NAME = "compose-multiplatform"
+
 public class ComposePlugin : Plugin<Project> {
 
     override fun apply(target: Project) {
         with(target) {
             project.pluginManager.withPlugin("org.jetbrains.compose") {
                 adjustResources()
-                // Trick to make assemble jvm resources task to work.
-                kotlin.targets.withType<KotlinJvmTarget> { adjustAssembleResTask(this) }
+                adjustAssembleJvmResTask()
             }
         }
     }
@@ -48,21 +71,60 @@ public class ComposePlugin : Plugin<Project> {
             else -> Unit
         }
 
+        adjustDesktopIcons(dir)
+    }
+
+    private fun Project.adjustDesktopIcons(dir: String) = pluginManager.withPlugin("org.jetbrains.compose.desktop") {
+        val drawableDir = project.file(dir).resolve("drawable")
+        val composeMultiplatformSvg = drawableDir.resolve("$COMPOSE_MULTIPLATFORM_ICON_NAME.svg")
+
+        if (!composeMultiplatformSvg.exists()) return@withPlugin
+
+        DENSITIES.forEach { (qualifier, size) ->
+            val densityDir = drawableDir.resolve("drawable-$qualifier")
+            densityDir.mkdirs()
+
+            val linuxIconFile = drawableDir.resolve("$COMPOSE_MULTIPLATFORM_ICON_NAME-linux.png")
+            val windowsIconFile = drawableDir.resolve("$COMPOSE_MULTIPLATFORM_ICON_NAME-windows.ico")
+            val macosIconFile = densityDir.resolve("$COMPOSE_MULTIPLATFORM_ICON_NAME.icns")
+
+            svgToPng(composeMultiplatformSvg, linuxIconFile, size)
+            Imaging.writeImage(ImageIO.read(linuxIconFile), windowsIconFile, ImageFormats.ICO)
+            linuxIconFile.copyTo(macosIconFile, overwrite = true)
+        }
+
+        val desktopDensityDir = drawableDir.resolve("drawable-${DENSITIES.keys.last()}")
+
         // jpackage only supports .png on Linux, .ico on Windows, .icns on Mac, so a developer must do a conversion (probably from a png) to a 3 different formats.
         // Also it seems that ico and icns need to contain an icon in multiple resolutions, so the conversion becomes a bit inconvenient.
         compose.desktop.application.nativeDistributions {
             macOS {
-                if (!iconFile.isPresent) iconFile = project.file("$dir/drawable/compose-multiplatform-osx.icns")
+                if (!iconFile.isPresent) iconFile = desktopDensityDir.resolve("$COMPOSE_MULTIPLATFORM_ICON_NAME-macos.icns")
             }
             linux {
-                if (!iconFile.isPresent) iconFile = project.file("$dir/drawable/compose-multiplatform-linux.png")
+                if (!iconFile.isPresent) iconFile = desktopDensityDir.resolve("$COMPOSE_MULTIPLATFORM_ICON_NAME-linux.png")
             }
             windows {
-                if (!iconFile.isPresent) iconFile = project.file("$dir/drawable/compose-multiplatform-mingw.ico")
+                if (!iconFile.isPresent) iconFile = desktopDensityDir.resolve("$COMPOSE_MULTIPLATFORM_ICON_NAME-windows.ico")
             }
         }
     }
 
+    private fun svgToPng(svgFile: File, pngFile: File, size: Int) {
+        FileInputStream(svgFile).use { input ->
+            val transcoder = PNGTranscoder().apply {
+                addTranscodingHint(PNGTranscoder.KEY_WIDTH, size.toFloat())
+                addTranscodingHint(PNGTranscoder.KEY_HEIGHT, size.toFloat())
+            }
+            val transcoderInput = TranscoderInput(input)
+            FileOutputStream(pngFile).use { output ->
+                transcoder.transcode(transcoderInput, TranscoderOutput(output))
+            }
+        }
+    }
+
+    // Trick to make assemble jvm resources task to work.
+    private fun Project.adjustAssembleJvmResTask() = kotlin.targets.withType<KotlinJvmTarget> { adjustAssembleResTask(this) }
     private fun Project.adjustAssembleResTask(target: KotlinTarget) {
         target.compilations.all { compilation ->
             val compilationResources = files(
