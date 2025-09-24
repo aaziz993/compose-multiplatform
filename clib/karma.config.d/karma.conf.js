70c6f03106dba0ded0b8a92413d507b572ea3b18
azizatoev <a.atoev93@gmail.com>
2025-09-24 16:49:32 +0500

    Initial Commit

--- clib/karma.config.d/karma.conf.js
+++ clib/karma.config.d/karma.conf.js
@@ -1,11 +1,11 @@
 /**
  *         Copyright 2025 Aziz Atoev
  * 
- * Licensed under the [project_license_name] (the "License");
+ * Licensed under the Apache-2.0 (the "License");
  * You may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  * 
- *         Apache License, Version 2.0
+ *         Apache-2.0
  * 
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
