9fa231394b8033025fa7232deef2cab05556ccc2
azizatoev <a.atoev93@gmail.com>
2025-09-25 19:16:40 +0500

    Initial Commit

--- appleApp/iosApp/ContentView.swift
+++ appleApp/iosApp/ContentView.swift
@@ -1,36 +1,19 @@
+import UIKit
 import SwiftUI
+import compose_app
 
-import SwiftUI
-
-struct Greeting {
-    func greet()->String {
-        return "Hello!"
+struct ComposeView: UIViewControllerRepresentable {
+    func makeUIViewController(context: Context) -> UIViewController {
+        MainViewControllerKt.MainViewController()
     }
 
-    func callBar() {
-        print("Calling Bar")
-    }
-
-    func callBazz() {
-        print("Calling Bazz")
-    }
+    func updateUIViewController(_ uiViewController: UIViewController, context: Context) {}
 }
 
 struct ContentView: View {
-    let greeting = Greeting()
-
     var body: some View {
-        VStack(spacing: 24) {
-            Text(greeting.greet())
-                .font(.title)
-            Button("Call Bar") {
-                greeting.callBar()
-            }
-            Button("Call Bazz") {
-                greeting.callBazz()
-            }
-        }
-        .padding()
+        ComposeView()
+            .ignoresSafeArea()
     }
 }
 
