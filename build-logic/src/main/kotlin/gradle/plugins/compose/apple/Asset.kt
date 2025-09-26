6e2d6fb73ab5ab554dc5903335d04424e25453c4
azizatoev <a.atoev93@gmail.com>
2025-09-26 15:16:07 +0500

    Initial Commit

--- build-logic/src/main/kotlin/gradle/plugins/compose/apple/Asset.kt
+++
@@ -1,9 +0,0 @@
-package gradle.plugins.compose.apple
-
-public interface Asset {
-
-    public val filename: String?
-    public val idiom: String
-    public val size: String
-    public val scale: String
-}
--- build-logic/src/main/kotlin/gradle/plugins/compose/apple/imageset/Image.kt
+++ build-logic/src/main/kotlin/gradle/plugins/compose/apple/imageset/Image.kt
@@ -1,14 +1,11 @@
 package gradle.plugins.compose.apple.imageset
 
-import gradle.plugins.compose.apple.Asset
 import kotlinx.serialization.Serializable
 
 @Serializable
 public data class Image(
-    override val filename: String? = null,
-    override val idiom: String,
-    override val size: String,
-    override val scale: String = "1x",
-    val platform: String,
+    val filename: String? = null,
+    val size: String,
+    val scale: String = "1x",
     val appearances: Set<Appearance> = setOf(Appearance()),
-) : Asset
+)
