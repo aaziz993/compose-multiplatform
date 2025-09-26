c508cfaae72385ad83e54fb61232ac74c55f869c
azizatoev <a.atoev93@gmail.com>
2025-09-26 16:38:18 +0500

    Initial Commit

--- server-app/src@jvm/Application.kt
+++ server-app/src@jvm/Application.kt
@@ -5,11 +5,12 @@ import config.ApplicationScript
 import config.ServerConfig
 import engine.Netty
 import io.github.smiley4.ktoropenapi.get
-import io.ktor.http.*
-import io.ktor.server.application.*
-import io.ktor.server.netty.*
-import io.ktor.server.response.*
-import io.ktor.server.routing.*
+import io.ktor.http.HttpStatusCode
+import io.ktor.server.application.Application
+import io.ktor.server.netty.NettyApplicationEngine
+import io.ktor.server.response.respondText
+import io.ktor.server.routing.Routing
+import io.ktor.server.routing.routing
 import kotlinx.coroutines.awaitCancellation
 
 public fun main(): Unit = SuspendApp {
