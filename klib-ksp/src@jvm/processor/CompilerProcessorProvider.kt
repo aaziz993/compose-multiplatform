3679eddb11acb8a59fc61ebd1b2158c4e1d0e183
azizatoev <a.atoev93@gmail.com>
2025-10-05 12:40:31 +0500

    Initial Commit

---
+++ klib-ksp/src@jvm/processor/CompilerProcessorProvider.kt
@@ -0,0 +1,31 @@
+/**
+ *         Copyright 2025 Aziz Atoev
+ * 
+ * Licensed under the [project_license_name] (the "License");
+ * You may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *         Apache License, Version 2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * 
+ */
+package processor
+
+import com.google.devtools.ksp.processing.SymbolProcessor
+import com.google.devtools.ksp.processing.SymbolProcessorEnvironment
+import com.google.devtools.ksp.processing.SymbolProcessorProvider
+
+public class CompilerProcessorProvider : SymbolProcessorProvider {
+
+  override fun create(environment: SymbolProcessorEnvironment): SymbolProcessor =
+    CompilerProcessor(
+      options = environment.options,
+      logger = environment.logger,
+      codeGenerator = environment.codeGenerator,
+    )
+}
