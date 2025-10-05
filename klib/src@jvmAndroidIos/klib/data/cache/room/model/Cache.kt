e7d02e6d7688b363f9f983fafd6c4676f45bfe8b
azizatoev <a.atoev93@gmail.com>
2025-10-05 16:57:05 +0500

    Initial Commit

--- klib/src@jvmAndroidIos/klib/data/cache/room/model/Cache.kt
+++ klib/src@jvmAndroidIos/klib/data/cache/room/model/Cache.kt
@@ -1,17 +1,15 @@
 package klib.data.cache.room.model
 
-import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.Fts4
 import androidx.room.Index
-import androidx.room.PrimaryKey
 
+// An FTS entity table always has a column named rowid that is the equivalent of an INTEGER PRIMARY KEY index.
+// Therefore, an FTS entity can only have a single field annotated with PrimaryKey, it must be named rowid and must be of INTEGER affinity.
+// The field can be optionally omitted in the class but can still be used in queries.
 @Entity(tableName = "cache", indices = [Index(value = ["key"], unique = true)])
 @Fts4
 public class Cache(
-    @PrimaryKey
-    @ColumnInfo(name = "rowid")
-    public val rowId: Int,
     public val key: String,
     public val value: String,
 )
