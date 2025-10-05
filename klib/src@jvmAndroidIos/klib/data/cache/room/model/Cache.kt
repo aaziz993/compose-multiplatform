136cc4c31ac944e473690c45eeef2e732f6cd463
azizatoev <a.atoev93@gmail.com>
2025-10-05 16:52:12 +0500

    Initial Commit

--- klib/src@jvmAndroidIos/klib/data/cache/room/model/Cache.kt
+++ klib/src@jvmAndroidIos/klib/data/cache/room/model/Cache.kt
@@ -1,13 +1,17 @@
 package klib.data.cache.room.model
 
+import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.Fts4
 import androidx.room.Index
 import androidx.room.PrimaryKey
 
-@Fts4
 @Entity(tableName = "cache", indices = [Index(value = ["key"], unique = true)])
+@Fts4
 public class Cache(
+    @PrimaryKey
+    @ColumnInfo(name = "rowid")
+    public val rowId: Int,
     public val key: String,
     public val value: String,
 )
