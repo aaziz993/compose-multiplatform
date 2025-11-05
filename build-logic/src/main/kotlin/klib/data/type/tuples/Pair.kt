cc2b661aa8305095bdae045b8e053868b19bdcb9
azizatoev <a.atoev93@gmail.com>
2025-11-05 23:06:06 +0500

    Initial Commit

--- build-logic/src/main/kotlin/klib/data/type/tuples/Pair.kt
+++ build-logic/src/main/kotlin/klib/data/type/tuples/Pair.kt
@@ -2,6 +2,8 @@ package klib.data.type.tuples
 
 public fun <T> T.pair(): Pair<T, T> = this to this
 
+public fun <A, B> A.toBy(other: B): Pair<B, A> = other to this
+
 public infix fun <A, B> A.to(second: (A) -> B): Pair<A, B> = this to second(this)
 
 public infix fun <A, B, C> Pair<A, B>.and(t3: C): Tuple3<A, B, C> = Tuple3(first, second, t3)
