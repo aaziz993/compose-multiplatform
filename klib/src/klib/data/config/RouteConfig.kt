d806e8f0f70bf7a1768e870187b54a5bd083b8d9
azizatoev <a.atoev93@gmail.com>
2025-12-05 06:41:40 +0500

    Initial Commit

--- klib/src/klib/data/config/RouteConfig.kt
+++ klib/src/klib/data/config/RouteConfig.kt
@@ -2,10 +2,12 @@ package klib.data.config
 
 import io.ktor.http.Url
 import klib.data.auth.AuthResource
+import klib.data.type.serialization.serializers.collections.SerializableAnyMap
 import kotlinx.serialization.Serializable
 
 @Serializable
 public data class RouteConfig(
     val urls: List<Url> = emptyList(),
+    public val metadata: SerializableAnyMap = emptyMap(),
     val authResource: AuthResource? = null,
 )
