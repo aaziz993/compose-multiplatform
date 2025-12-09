c5133ce5f820fcc6a04b737fd1fca80d3420b0e3
azizatoev <a.atoev93@gmail.com>
2025-12-09 18:27:04 +0500

    Initial Commit

--- clib/src/clib/presentation/components/loading/LoadingCircle.kt
+++ clib/src/clib/presentation/components/loading/LoadingCircle.kt
@@ -8,7 +8,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 
 @Composable
-public fun LoadingCircle(modifier: Modifier = Modifier): Unit = Box(
+public fun CenterCircularProgressIndicator(modifier: Modifier = Modifier): Unit = Box(
     Modifier.fillMaxSize(),
     Alignment.Center,
 ) {
--- compose-app/src/ui/services/ServicesScreen.kt
+++ compose-app/src/ui/services/ServicesScreen.kt
@@ -1,9 +1,25 @@
 package ui.services
 
+import androidx.compose.foundation.layout.Row
+import androidx.compose.material.icons.Icons
+import androidx.compose.material3.Icon
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import clib.presentation.navigation.NavigationAction
 import androidx.compose.ui.tooling.preview.Preview
+import clib.presentation.icons.Amazon
+import clib.presentation.icons.Cloudflare
+import clib.presentation.icons.Dropbox
+import clib.presentation.icons.Facebook
+import clib.presentation.icons.Github
+import clib.presentation.icons.Gitlab
+import clib.presentation.icons.Google
+import clib.presentation.icons.Instagram
+import clib.presentation.icons.Microsoft
+import clib.presentation.icons.OneDrive
+import clib.presentation.icons.OpenOtp
+import clib.presentation.icons.Reddit
+import clib.presentation.icons.Twitter
 import ui.navigation.presentation.Services
 
 @Composable
@@ -12,6 +28,21 @@ public fun ServicesScreen(
     route: Services = Services,
     onNavigationAction: (NavigationAction) -> Unit = {},
 ) {
+    Row {
+        Icon(Icons.Default.Amazon, "Amazon")
+        Icon(Icons.Default.Cloudflare, "Cloudflare")
+        Icon(Icons.Default.Dropbox, "Dropbox")
+        Icon(Icons.Default.Facebook, "Facebook")
+        Icon(Icons.Default.Github, "Github")
+        Icon(Icons.Default.Gitlab, "Gitlab")
+        Icon(Icons.Default.Google, "Google")
+        Icon(Icons.Default.Instagram, "Instagram")
+        Icon(Icons.Default.Microsoft, "Microsoft")
+        Icon(Icons.Default.OneDrive, "OneDrive")
+        Icon(Icons.Default.OpenOtp, "OpenOtp")
+        Icon(Icons.Default.Reddit, "Reddit")
+        Icon(Icons.Default.Twitter, "Twitter")
+    }
 }
 
 @Preview
