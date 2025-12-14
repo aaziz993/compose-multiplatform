843ba76f4fadf3b33c1e4633df9cabe22724242a
azizatoev <a.atoev93@gmail.com>
2025-12-14 13:24:09 +0500

    Initial Commit

---
+++ klib/test/klib/data/type/primitives/string/case/ComplexTest.kt
@@ -0,0 +1,90 @@
+/*
+ * Copyright © 2019-2020, PearX Team
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this file,
+ * You can obtain one at https://mozilla.org/MPL/2.0/.
+ */
+
+package klib.data.type.primitives.string.case
+
+import klib.data.type.primitives.string.case.formatter.CaseFormatterConfig
+import klib.data.type.primitives.string.case.splitter.WordSplitterConfig
+import kotlin.test.Test
+import kotlin.test.assertEquals
+
+class ComplexTest {
+    @Test
+    fun testCamel() {
+        assertEquals("xmlHttpRequestV2Updated", "xml_http_request_v2_updated".toCamelCase(CaseFormat.LOWER_UNDERSCORE))
+    }
+
+    @Test
+    fun testPascal() {
+        assertEquals("XmlHttpRequestV2Updated", "xml_http_request_v2_updated".toPascalCase(CaseFormat.LOWER_UNDERSCORE))
+    }
+
+    @Test
+    fun testSnake() {
+        assertEquals("xml_http_request_v2_updated", "XML_HTTP_REQUEST_V2_UPDATED".toSnakeCase(CaseFormat.UPPER_UNDERSCORE))
+    }
+
+    @Test
+    fun testScreamingSnake() {
+        assertEquals("XML_HTTP_REQUEST_V2_UPDATED", "xml_http_request_v2_updated".toScreamingSnakeCase(CaseFormat.LOWER_UNDERSCORE))
+    }
+
+    @Test
+    fun testKebab() {
+        assertEquals("xml-http-request-v2-updated", "xml_http_request_v2_updated".toKebabCase(CaseFormat.LOWER_UNDERSCORE))
+    }
+
+    @Test
+    fun testTrain() {
+        assertEquals("XML-HTTP-REQUEST-V2-UPDATED", "xml_http_request_v2_updated".toTrainCase(CaseFormat.LOWER_UNDERSCORE))
+    }
+
+    @Test
+    fun testLowerSpace() {
+        assertEquals("xml http request v2 updated", "xml_http_request_v2_updated".toLowerSpaceCase(CaseFormat.LOWER_UNDERSCORE))
+    }
+
+    @Test
+    fun testUpperSpace() {
+        assertEquals("XML HTTP REQUEST V2 UPDATED", "xml_http_request_v2_updated".toUpperSpaceCase(CaseFormat.LOWER_UNDERSCORE))
+    }
+
+    @Test
+    fun testTitle() {
+        assertEquals("Xml Http Request V2 Updated", "xml_http_request_v2_updated".toTitleCase(CaseFormat.LOWER_UNDERSCORE))
+    }
+
+    @Test
+    fun testSentence() {
+        assertEquals("Xml http request v2 updated", "xml_http_request_v2_updated".toSentenceCase(CaseFormat.LOWER_UNDERSCORE))
+    }
+
+    @Test
+    fun testDot() {
+        assertEquals("xml.http.request.v2.updated", "xml_http_request_v2_updated".toDotCase(CaseFormat.LOWER_UNDERSCORE))
+    }
+
+    @Test
+    fun testDotUpper() {
+        assertEquals("XML.HTTP.REQUEST.V2.UPDATED", "xml_http_request_v2_updated".toUpperDotCase(CaseFormat.LOWER_UNDERSCORE))
+    }
+
+    @Test
+    fun testCustom1() {
+        assertEquals("My**Xml**Request**V2**Updated", "MY XML-REQUEST.V2_UPDATED".toCase(CaseFormatterConfig(false, "**", wordCapitalize = true, firstWordCapitalize = true), universalWordSplitter(false)))
+    }
+
+    @Test
+    fun testCustom2() {
+        assertEquals("My**Xml**Request**V2**Updated", "MY_XML_REQUEST_V2_UPDATED".toCase(CaseFormatterConfig(false, "**", wordCapitalize = true, firstWordCapitalize = true), CaseFormat.UPPER_UNDERSCORE))
+    }
+
+    @Test
+    fun testCustom3() {
+        assertEquals("My**Xml**Request**V2**Updated", "my*xml*request*v2*updated".toCase(CaseFormatterConfig(false, "**", wordCapitalize = true, firstWordCapitalize = true), WordSplitterConfig(boundaries = setOf('*'))))
+    }
+}
---
+++ klib/test/klib/data/type/primitives/string/case/UniversalWordSplitter2Test.kt
@@ -0,0 +1,88 @@
+/*
+ * Copyright © 2019-2020, PearX Team
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this file,
+ * You can obtain one at https://mozilla.org/MPL/2.0/.
+ */
+
+package klib.data.type.primitives.string.case
+
+import kotlin.test.Test
+import kotlin.test.assertEquals
+
+class UniversalWordSplitter2Test {
+    @Test
+    fun testCamel() {
+        assertEquals(listOf("xml", "Http", "Request", "V2", "Updated"), "xmlHttpRequestV2Updated".splitToWords(universalWordSplitter(false)))
+    }
+
+    @Test
+    fun testPascal() {
+        assertEquals(listOf("XML", "Http", "Request", "V2", "Updated"), "XMLHttpRequestV2Updated".splitToWords(universalWordSplitter(false)))
+    }
+
+    @Test
+    fun testSnake() {
+        assertEquals(listOf("xml", "http", "request", "v2", "updated"), "xml_http_request_v2_updated".splitToWords(universalWordSplitter(false)))
+    }
+
+    @Test
+    fun testScreamingSnake() {
+        assertEquals(listOf("XML", "HTTP", "REQUEST", "V2", "UPDATED"), "XML_HTTP_REQUEST_V2_UPDATED".splitToWords(universalWordSplitter(false)))
+    }
+
+    @Test
+    fun testKebab() {
+        assertEquals(listOf("xml", "http", "request", "v2", "updated"), "xml-http-request-v2-updated".splitToWords(universalWordSplitter(false)))
+    }
+
+    @Test
+    fun testTrain() {
+        assertEquals(listOf("XML", "HTTP", "REQUEST", "V2", "UPDATED"), "XML-HTTP-REQUEST-V2-UPDATED".splitToWords(universalWordSplitter(false)))
+    }
+
+    @Test
+    fun testLowerSpace() {
+        assertEquals(listOf("xml", "http", "request", "v2", "updated"), "xml http request v2 updated".splitToWords(universalWordSplitter(false)))
+    }
+
+    @Test
+    fun testUpperSpace() {
+        assertEquals(listOf("XML", "HTTP", "REQUEST", "V2", "UPDATED"), "XML HTTP REQUEST V2 UPDATED".splitToWords(universalWordSplitter(false)))
+    }
+
+    @Test
+    fun testTitle() {
+        assertEquals(listOf("XML", "Http", "Request", "V2", "Updated"), "XML Http Request V2 Updated".splitToWords(universalWordSplitter(false)))
+    }
+
+    @Test
+    fun testSentence() {
+        assertEquals(listOf("Xml", "http", "request", "v2", "updated"), "Xml http request v2 updated".splitToWords(universalWordSplitter(false)))
+    }
+
+    @Test
+    fun testDot() {
+        assertEquals(listOf("xml", "http", "request", "v2", "updated"), "xml.http.request.v2.updated".splitToWords(universalWordSplitter(false)))
+    }
+
+    @Test
+    fun testDotUpper() {
+        assertEquals(listOf("XML", "HTTP", "REQUEST", "V2", "UPDATED"), "XML.HTTP.REQUEST.V2.UPDATED".splitToWords(universalWordSplitter(false)))
+    }
+
+    @Test
+    fun testMulti1() {
+        assertEquals(listOf("MY", "XML", "REQUEST", "V2", "UPDATED"), "MY XML-REQUEST.V2_UPDATED".splitToWords(universalWordSplitter(false)))
+    }
+
+    @Test
+    fun testMulti2() {
+        assertEquals(listOf("my", "xml", "request", "v2", "updated"), "my xml-request.v2_updated".splitToWords(universalWordSplitter(false)))
+    }
+
+    @Test
+    fun testMulti3() {
+        assertEquals(listOf("my", "XML", "Request", "v2", "UPDATED"), "my XML-Request.v2_UPDATED".splitToWords(universalWordSplitter(false)))
+    }
+}
