9d27e40edfd5219a30bf221167532d7f9ecfff72
azizatoev <a.atoev93@gmail.com>
2025-12-16 17:02:40 +0500

    Initial Commit

--- clib/src/clib/presentation/navigation/BaseRoute.kt
+++ clib/src/clib/presentation/navigation/BaseRoute.kt
@@ -89,7 +89,7 @@ public sealed class BaseRoute : Iterable<BaseRoute> {
                 { router.onClick(this@BaseRoute) },
                 selectedItem.icon,
                 selectedItem.modifier,
-                false,
+                enabled,
                 selectedItem.text,
                 alwaysShowLabel,
                 selectedItem.badge,
