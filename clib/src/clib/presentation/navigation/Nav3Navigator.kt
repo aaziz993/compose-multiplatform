2555fbf9811616c8b89a2cd310bbbf43b724aff8
azizatoev <a.atoev93@gmail.com>
2025-12-22 23:16:22 +0500

    Initial Commit

--- clib/src/clib/presentation/navigation/Nav3Navigator.kt
+++ clib/src/clib/presentation/navigation/Nav3Navigator.kt
@@ -316,8 +316,9 @@ public fun rememberNav3Navigator(
     authRoute: NavRoute? = null,
     authRedirectRoute: NavRoute? = null,
     onError: (Throwable) -> Unit = { e -> throw e },
-    onUnknownRoute: (NavRoute) -> Unit = LocalRouter.current?.let { router -> { router.push(it) } }
-        ?: { navRoute -> onError(IllegalStateException("Unknown route '$navRoute'")) },
+    onUnknownRoute: (NavRoute) -> Unit = LocalRouter.current?.let { router ->
+        { navRoute -> router.push(navRoute) }
+    } ?: { navRoute -> onError(IllegalStateException("Unknown route '$navRoute'")) },
     onBack: () -> Unit = LocalRouter.current?.let { it::pop } ?: platformOnBack(),
 ): Navigator {
     require(startRoute?.let { it.route in routes } != false) { "Start route '$startRoute' not in '$routes'" }
