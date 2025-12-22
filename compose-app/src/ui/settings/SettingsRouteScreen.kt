6d460660f7b5fa079b2116977e5978debe991a2d
azizatoev <a.atoev93@gmail.com>
2025-12-22 22:26:50 +0500

    Initial Commit

--- compose-app/src/ui/settings/SettingsRouteScreen.kt
+++ compose-app/src/ui/settings/SettingsRouteScreen.kt
@@ -101,7 +101,7 @@ private fun SettingsRouteConfig(
 
     metadataList.forEach { metadata ->
         SettingsSwitch(
-            title = metadata.keys.map { it.asStringResource() }.joinToString(LINE_SEPARATOR),
+            title = metadata.keys.map { it.asStringResource() }.joinToString(String.LINE_SEPARATOR),
             metadata.keys.all { key -> key in value.metadata || key in value.additionalMetadata },
             trueIcon = Icons.Filled.DisplaySettings,
             falseIcon = Icons.Outlined.DisplaySettings,
